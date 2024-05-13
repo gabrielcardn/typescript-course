de0e30b1cdfc1499602d11cd29259445dd9f6268
Gabriel-PC <gabrielcard2001@gmail.com>
2024-05-13 02:43:39 -0300

    removed build folders

--- sort/build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Sorter_1 = require("./Sorter");
-const NumbersCollection_1 = require("./NumbersCollection");
-const numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 100]);
-const sorter = new Sorter_1.Sorter(numbersCollection);
-console.log("Before: ", sorter.collection);
-sorter.sort();
-console.log("After: ", sorter.collection);
