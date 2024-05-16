aa8268dfb8f31aa169a22f1dbea3afff7811ef01
Gabriel-PC <gabrielcard2001@gmail.com>
2024-05-15 22:39:41 -0300

    created a Summary class, and some more refactoring

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -3,13 +3,15 @@ import { CsvFileReader } from "./CsvFileReader";
 import { Summary } from "./Summary";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 
-const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader.fromCsv("football.csv");
 matchReader.load();
 
-const summary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport());
+const summary = Summary.winsAnalysisWithHtmlReport("Man United");
+//new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 
 summary.buildAndPrintReport(matchReader.matches);
 
